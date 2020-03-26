de77412d40bcecc93cd9ffb218c482959c47dd38
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-03-26 02:25:50 +0000

    Completed First Option of Menu

--- Functions.h
+++ Functions.h
@@ -7,6 +7,7 @@
 using namespace std;
 
 void showMenu();
+void chosenOption(string);
 
 void showMenu()
 {
@@ -17,8 +18,53 @@ void showMenu()
   cout << "D: Newton's Second Law" << endl;
   cout << "E: Weight(Earth)" << endl;
   cout << "F: Momentum" << endl;
+  cout << "X: Exit " << endl;
+  cout << "Z: To CLear The Scren" << endl;
+}
+void chosenOption(string option)
+{
+  string color = "\x1b[" + to_string(31) + ";1m";
+  string reset = "\x1b[0m";
+
+  if(option == "A" || option == "a")
+  {
+    double ds,dt = 0.0;
+    string dsUnits, dtUnits;
+    cout << color;
+    cout << "\nVelocity Calculator" << endl;
+    cout << reset;
+    cout << "\nWhat is your distance?" << endl;
+    ds = validateDouble(ds);
+    cout << "\nWhat is the unit of measure for distance? (km,m,cm,miles,feet,inch)" << endl;
+    dsUnits = validateString(dsUnits);
+    cout << "\nWhat is your time?" << endl;
+    dt = validateDouble(dt);
+    cout << "\nWhat is the unit of measure for time? (hour,min,sec)" << endl;
+    dtUnits = validateString(dtUnits);
+
+    double V = ds / dt;
+
+    cout << ds << " / " << dt << " = " << V << " " << dsUnits << "/" << dtUnits << endl;
 
 
+  }
+  else if(option == "B" || option == "b")
+  {
 
+
+  }
+   else if(option == "Z" || option == "z")
+  {
+    string reset = "\x1b[0m";
+    cout << reset;
+    cout << "\033[2J\033[1;1H";
+    system("clear");
+
+  }
+  else
+  {
+    cout << "\nInvalid Option Try Again." << endl;
+  }
+  
 
 }
--- main.cpp
+++ main.cpp
@@ -1,33 +1,25 @@
 //Student: Orlando Valdez
 //Teacher: Dr_T
-//03-24-2020
+//03-20-20
 
 #include "Functions.h"
 
-
-int main()
+int main ()
 {
   string option = "";
-
-  string color = "\x1b[" + to_string(32) + ";1m";
-
+  string colorOption = "\x1b[" + to_string(32) + ";1m";//green text
   string reset = "\x1b[0m";
-
   do
   {
     showMenu();
-    //call function and ask for user input
-    cout << "\Enter an option(A-F): ";
-    cout << color;
-    option = validateString(option);
-    cout << reset; //resets the string
+    cout << "\nPlease Select An Option: " ;
+    cout << colorOption;// changes text color to green
+    option = validateString(option);// validates user input to correct input
+    cout << reset;//resets the text color back to default
+    chosenOption(option);
 
-
-
-    
-  }while(option != "e"  );
-    
-
-
+  
+  }while(option != "X" && option != "x");
+   cout << "\nAdios!" << endl;
   return 0;
 }
