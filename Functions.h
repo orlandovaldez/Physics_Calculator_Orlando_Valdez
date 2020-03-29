29d314b1896abb3c160e42d0201232a110727a89
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-03-29 07:52:23 +0000

    Added Weight(Earth) Function

--- Functions.h
+++ Functions.h
@@ -8,6 +8,11 @@ using namespace std;
 
 void showMenu();
 void chosenOption(string);
+void calculateVelocity();
+void calculateAcceleration();
+void calculateForce();
+void calculateWeight();
+
 
 void showMenu()
 {
@@ -23,51 +28,13 @@ void showMenu()
 }
 void chosenOption(string option)
 {
-  string color = "\x1b[" + to_string(31) + ";1m";
-  string reset = "\x1b[0m";
-
   if(option == "A" || option == "a")
   {
-    double ds,dt = 0.0;
-    string dsUnits, dtUnits;
-    cout << color;
-    cout << "\nVelocity Calculator" << endl;
-    cout << reset;
-    cout << "\nWhat is your distance?" << endl;
-    ds = validateDouble(ds);
-    cout << "\nWhat is the unit of measure for distance? (km,m,cm,miles,feet,inch)" << endl;
-    dsUnits = validateString(dsUnits);
-    cout << "\nWhat is your time?" << endl;
-    dt = validateDouble(dt);
-    cout << "\nWhat is the unit of measure for time? (hour,min,sec)" << endl;
-    dtUnits = validateString(dtUnits);
-
-    double V = ds / dt;
-
-    cout << ds << " / " << dt << " = " << V << " " << dsUnits << "/" << dtUnits << endl;
-
-
+    calculateVelocity();
   }
   else if(option == "B" || option == "b")
   {
-    double dv,dt = 0.0;
-    string dvUnits, dtUnits;
-    cout << color;
-    cout << "\nAcceleration Calculator" << endl;
-    cout << reset;
-    cout << "\nWhat is your change in velocity?" << endl;
-    dv = validateDouble(dv);
-    cout << "\nWhat is the unit of measure for velocity? (km,m,cm,miles,feet,inch)" << endl;
-    dvUnits = validateString(dvUnits);
-    cout << "\nWhat is your time?" << endl;
-    dt = validateDouble(dt);
-    cout << "\nWhat is the unit of measure for time? (hour,min,sec)" << endl;
-    dtUnits = validateString(dtUnits);
-
-    double V = dv / dt;
-
-    cout << dv << " / " << dt << " = " << V << " " << dvUnits << "/" << dtUnits << endl;
-
+    calculateAcceleration();
   }
   else if(option == "C" || option == "c")
   {
@@ -77,15 +44,11 @@ void chosenOption(string option)
   }
   else if(option == "D" || option == "d")
   {
-
-    cout << "\nNewton's Second Law Calculator " << endl;
-
+    calculateForce();
   }
   else if(option == "E" || option == "e")
   {
-
-    cout << "\nWeight(Earth) Calculator" << endl;
-
+    calculateWeight();
   }
   else if(option == "F" || option == "f")
   {
@@ -106,5 +69,94 @@ void chosenOption(string option)
     cout << "\nInvalid Option Try Again." << endl;
   }
   
+
+}
+void calculateVelocity()
+{
+    string color = "\x1b[" + to_string(31) + ";1m";
+    string reset = "\x1b[0m";
+    double ds,dt = 0.0;
+    string dsUnits, dtUnits;
+    cout << color;
+    cout << "\nVelocity Calculator" << endl;
+    cout << reset;
+    cout << "\nWhat is your distance?" << endl;
+    ds = validateDouble(ds);
+    cout << "\nWhat is the unit of measure for distance? (km,m,cm,miles,feet,inch)" << endl;
+    dsUnits = validateString(dsUnits);
+    cout << "\nWhat is your time?" << endl;
+    dt = validateDouble(dt);
+    cout << "\nWhat is the unit of measure for time? (hour,min,sec)" << endl;
+    dtUnits = validateString(dtUnits);
+
+    double V = ds / dt;
+
+    cout << ds << " / " << dt << " = " << V << " " << dsUnits << "/" << dtUnits << endl;
+}
+void calculateAcceleration()
+{
+  string color = "\x1b[" + to_string(31) + ";1m";
+  string reset = "\x1b[0m";
+  double dv,dt = 0.0;
+  string dvUnits, dtUnits;
+  cout << color;
+  cout << "\nAcceleration Calculator" << endl;
+  cout << reset;
+  cout << "\nWhat is your change in velocity?" << endl;
+  dv = validateDouble(dv);
+  cout << "\nWhat is the unit of measure for velocity? (km,m,cm,miles,feet,inch)" << endl;
+  dvUnits = validateString(dvUnits);
+  cout << "\nWhat is your time?" << endl;
+  dt = validateDouble(dt);
+  cout << "\nWhat is the unit of measure for time? (hour,min,sec)" << endl;
+  dtUnits = validateString(dtUnits);
+
+  double V = dv / dt;
+
+  cout << dv << " / " << dt << " = " << V << " " << dvUnits << "/" << dtUnits << endl;
+}
+void calculateForce()
+{
+  string color = "\x1b[" + to_string(31) + ";1m";
+  string reset = "\x1b[0m";
+  
+  double m,a = 0.0;
+  string mUnits, aUnits;
+  cout << color;
+  cout << "\nNewton's Second Law (Force) Calculator" << endl;
+  cout << reset;
+  cout << "\nWhat is the Mass? " << endl;
+  m = validateDouble(m);
+  cout << "\nWhat is the Mass Unit of Measure?(kg or lb)" << endl;
+  mUnits = validateString(mUnits);
+  cout << "\nWhat is the rate of Acceleration? " << endl;
+  a = validateDouble(a);
+  cout << "\nWhat is the Acceleration Unit of Measure?(m,cm,ft,in.)" << endl;
+  aUnits =validateString(aUnits);
+  
+  double N = ( m * a );
+  cout << m << " * " << a << " = " << color << N << reset << " " << mUnits << " " << aUnits << "/s^2" << endl; 
+  
+}
+void calculateWeight()
+{
+  string color = "\x1b[" + to_string(31) + ";1m";
+  string reset = "\x1b[0m";
+
+  double m,w = 0.0;//initializes mass and weight for varibales as doubles
+  double g = 9.8;//sets the gravity rate for Earth at 9.8 m/s
+  string mUnits, gUnits;
+
+  cout << color;
+  cout << "\nWeight(Earth) Calculator" << endl;
+  cout << reset;
+  cout << "\nWhat is the initial mass? " << endl;
+  m = validateDouble(m);
+  cout << "\nWhat is the Unit of Measure Output?(ie. lbs,stone) " << endl;
+  mUnits =validateString(mUnits);
+  w = m * g;
+  cout << "\nMass * Gravity = Weight " << endl;
+  cout << m << " * " << g << " = " << color << w << reset << mUnits << endl;
+
 
 }
